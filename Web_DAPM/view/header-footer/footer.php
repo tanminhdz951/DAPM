125e12e7fd109a5e9bfb0c1cf518790f3d2c3317
NgocHieu <lengochieu732001@gmail.com>
2022-06-20 10:04:49 +0700

    Message

---
+++ Web_DAPM/view/header-footer/footer.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cứu trợ lũ lụt</title>
+    <link rel="stylesheet" href="view/css/style.css">
+    
+    <link href="view/nguoidung/asset/css/bootstrap.min.css" rel="stylesheet">   
+    
+    <!-- Font Awesome CSS -->
+    <link href="view/nguoidung/css/font-awesome.min.css" rel="stylesheet">
+    
+
+    <link href="view/nguoidung/css/style.css" rel="stylesheet">
+   
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    
+    <!-- Colors CSS -->
+  
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    
+    
+    <!-- Modernizer js -->
+    <script src="view/nguoidung/js/modernizr.custom.js"></script>
+
+</head>
+<body>
+    <footer id="footer" >
+        <div class="col1">
+            <div>
+                <br>
+                <img id="logo-img" src="https://sos24h.vn/wwwroot/resources/upload/img/productcategory/8-2020/logo2.png" alt="logo" width="70" height="70"><br>
+            </div>
+        </div>
+        <div class="col2">
+            <ul>
+                <li><a href="view/ungho/dangky_ungho.php">Đăng kí ủng hộ</a></li>
+                <li><a href="view/ungho/danhsach_ungho.php">Danh sách ủng hộ</a></li>
+            </ul>
+        </div>
+        <div class="col3">
+            <ul>
+                <li><a>Đại học sư phạm kĩ thuật Đà Nẵng</a></li>
+                <li><a>Địa chỉ: 48 Cao Thắng Hải Châu Đà Nẵng</a></li>
+                <li><a>Email: Dinhvuong98@gmail.com</a></li>
+                <li><a>SDT: 0763726790</a></li>
+            </ul>
+        </div>
+        <div class="col4">
+            <ul>
+                <img src="https://sos24h.vn/wwwroot/resources/upload/img/productcategory/8-2020/logo2.png" alt="" width="120" height="120">
+
+
+            </ul>
+        </div>
+        <div class="footer-top">
+            <div class="container2">
+                <div class="row2">
+
+                    <div class="col-lg-42 col-md-6 footer-info">
+                        <p><b>Cứu trợ, nhịp cầu nối liền những trái tim</b></p>
+                    </div>
+
+                    <div class="col-lg-22 col-md-6 footer-links">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-phone" >Hotline: 0363907251</i></a></li>
+                        
+                        </ul>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+      
+  </footer><!-- End Footer -->
+</body>
+</html>
